2a05a41f530abbcb248bb2438ea383c8ab8d8642
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-09-21 05:28:25 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Timer from './Timer';
+import '../App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Split Timer</h1>
+      </header>
+      <Timer />
+    </div>
+  );
+}
